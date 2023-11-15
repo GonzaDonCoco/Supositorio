eee11aea33d3dc6114a450341185f6dee61cb01b
GonzaDonCoco <gonz.pena@duocuc.cl>
2023-11-15 16:10:15 -0300

    a

--- claseArreglos.py
+++ claseArreglos.py
@@ -1,9 +1,11 @@
 import numpy as np
+import random as r
+
+"""
 
 #Arreglo unidimensional de tamaño 10 con elementos aleatorios enteros del 0 al 100
 arreglo1=np.random.randint(100, size=10)
 print(arreglo1)
-
 
 #copiar arreglo
 arreglo2=arreglo1
@@ -24,10 +26,19 @@ print('Promedio de los elementos')
 print(arreglo2.mean())
 print('**************')
 #Mostrar todos los elementos
-print('Mostrar todos los elementos')
-print(arreglo2)
+for i in range(0,len(arreglo2)):
+    print(f"Numero {i} es {arreglo2[i]}")
 
+"""
 
+#Arreglos Bidimensionales
 
+lista=[range(100)]
 
+#for i in range (100):
+#    lista.append(r.randint(0,100))
 
+matriz=np.zeros()
+
+print(lista)
+print(matriz)
