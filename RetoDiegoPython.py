509f1ef342dae5648fdf390567e99238ead2980c
GonzaDonCoco <gonz.pena@duocuc.cl>
2023-11-15 16:11:24 -0300

    banana

--- RetoDiegoPython.py
+++ RetoDiegoPython.py
@@ -12,7 +12,7 @@ import numpy as np
         print('Email válido')
     else:
         print('Email inválido')
-
+"""
 #Contraseña de 6 dígitos
 def contraseñas():
     caracteres='abcdefghijklmnopqrstuvwxyz1234567890'
@@ -23,18 +23,28 @@ def contraseñas():
     print(contraseña)
 
 
-#Numeros de 1 a 1000 donde los pares printean "Fizz" e impares "Buzz"
+#Numeros de 1 a 1000 donde los pares printean "Fizz", impares "Buzz" y primos "FizzBuzz"
 def FizzBuzz():
     for i in range (1,1000):
+        print(i)
         if i%2==0:
             print('Par')
-        elif i%2!=0:
-            print('impar')
         else:
-            print('Chimbarongo')
+            print('Impar')
+        div = 0 
+        for x in range(2,i):
+            if(i%x==0):
+                div+=1
+        if(div==0):
+            print("Primo")
+        
             
 
+            
+FizzBuzz()
 """
+#Otra forma de verificar el correo
+
 def validar_correo_electronico(correo):
     # Patrón de expresión regular para validar direcciones de correo electrónico
     patron = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
@@ -52,3 +62,4 @@ if validar_correo_electronico(correo_ejemplo):
     print(f"La dirección de correo electrónico {correo_ejemplo} es válida.")
 else:
     print(f"La dirección de correo electrónico {correo_ejemplo} no es válida.")
+"""
